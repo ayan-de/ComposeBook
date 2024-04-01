e8f5c2944788978ef55c69fecf4ee3151551e657
Ayan De <deayan252@gmail.com>
2024-04-01 23:13:35 +0530

    "`Name` attribute changed to `text`"

--- app/src/main/java/com/example/composebookapp/ui/theme/RowItemData.kt
+++ app/src/main/java/com/example/composebookapp/ui/theme/RowItemData.kt
@@ -6,6 +6,6 @@ val rowItemData: List<RowItems> = mutableListOf(
     RowItems("onClick","Enter onClick functionality"),
     RowItems("color","enter the color"),
     RowItems("size","enter the size"),
-    RowItems("Name","enter the name"),
+    RowItems("text","enter the text"),
     RowItems("icon","")
 )
