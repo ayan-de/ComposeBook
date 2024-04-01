709557597d4da6b365817c8adb807874aaa26e88
Ayan De <deayan252@gmail.com>
2024-04-01 23:06:17 +0530

    "unused imports removed from TableScreen.kt file"

--- app/src/main/java/com/example/composebookapp/ui/theme/TableScreen.kt
+++ app/src/main/java/com/example/composebookapp/ui/theme/TableScreen.kt
@@ -1,21 +1,15 @@
 package com.example.composebookapp.ui.theme
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-//import androidx.compose.foundation.layout.FlowRowScopeInstance.weight
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
