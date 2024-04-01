86c5c8a73a7504c1e3b74394327ad0526c2cc408
Ayan De <deayan252@gmail.com>
2024-04-02 01:01:56 +0530

    "unused imports and warning removed , var changed to var"

--- app/src/main/java/com/example/composebookapp/MainActivity.kt
+++ app/src/main/java/com/example/composebookapp/MainActivity.kt
@@ -6,15 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.composebookapp.ui.theme.ComponentScreen
 import com.example.composebookapp.ui.theme.ComposeBookAppTheme
 
@@ -22,7 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        var darkMode by mutableStateOf(true)
+        val darkMode by mutableStateOf(true)
 
         setContent {
             ComposeBookAppTheme(darkTheme = darkMode) {
@@ -38,4 +32,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
