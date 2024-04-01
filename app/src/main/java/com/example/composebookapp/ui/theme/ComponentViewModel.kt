80e2befe7d394a3c62d514efbaf9eb1b36e47874
Ayan De <deayan252@gmail.com>
2024-04-02 00:56:56 +0530

    "mutableStateOf replaced with mutableIntStateOf"

--- app/src/main/java/com/example/composebookapp/ui/theme/ComponentViewModel.kt
+++ app/src/main/java/com/example/composebookapp/ui/theme/ComponentViewModel.kt
@@ -1,12 +1,13 @@
 package com.example.composebookapp.ui.theme
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class ComponentViewModel : ViewModel() {
-    var selectedTabIndex by mutableStateOf(0)
+    var selectedTabIndex by mutableIntStateOf(0)
 
     var darkMode by
     mutableStateOf(false)
